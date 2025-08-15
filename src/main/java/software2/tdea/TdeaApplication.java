efe4441af4ff89dbfad3cf12a4f0ce6abcc4b5cb
SamuelGoHE <samuel.gomezhenao@gmail.com>
2025-08-14 21:24:59 -0500

    Primer commit - Proyecto Spring Boot

---
+++ src/main/java/software2/tdea/TdeaApplication.java
@@ -0,0 +1,13 @@
+package software2.tdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/software2/tdea/TdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package software2.tdea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
